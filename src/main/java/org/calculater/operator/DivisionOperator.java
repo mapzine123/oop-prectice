ca96e1aec3821df411496f47d422c37e51124126
un <mapzine123@naver.com>
2024-06-27 00:31:01 +0900

    # 0으로 나눌 수 없는 요구사항 처리

--- src/main/java/org/calculater/operator/DivisionOperator.java
+++ src/main/java/org/calculater/operator/DivisionOperator.java
@@ -1,5 +1,11 @@
 package org.calculater.operator;
 
+/**
+ * 0으로 나누면 예외 발생 상황 처리
+ * 응집도가 높기 때문에, DivisionOperator 클래스만 수정하면 됨
+ * 다른 operator에는 영향 없음
+ */
+
 public class DivisionOperator implements NewArithmeticOperator {
     @Override
     public boolean supports(String operator) {
@@ -8,6 +14,9 @@ public class DivisionOperator implements NewArithmeticOperator {
 
     @Override
     public int calculate(int operand1, int operand2) {
+        if(operand2 == 0) {
+            throw new IllegalArgumentException("0으로는 나눌 수 없습니다.");
+        }
         return operand1 / operand2;
     }
 }
